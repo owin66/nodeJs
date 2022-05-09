4eefef10b66c0faeb08ebacf99f025c57397dab7
owin66 <78900270+owin66@users.noreply.github.com>
2022-05-09 13:34:33 +0300

    command_nodeJs_object_module_os

--- modules.js
+++ modules.js
@@ -1,5 +1,11 @@
-const {userName: user,sayHi} = require('./test')
+const os = require('os')
+
+
+const {userName: user, sayHi} = require('./test')
 
 const name = 'Tommy'
 
 console.log(sayHi(name))
+console.log(os.platform(), os.release())
+
+module.exports = name
